bcfa7e60cbee9d0f7673b7b4058ce597f24d438a
Kutaeva_Yuliya <kutaeva.yuliya@gmail.com>
2023-10-12 04:14:33 +0300

    rjvvb

--- src/homework/day2/cycletask/PrintNumbers.java
+++ src/homework/day2/cycletask/PrintNumbers.java
@@ -3,7 +3,10 @@ package homework.day2.cycletask;
 public class PrintNumbers{
     public void printNum(){
         int i=0;
-        while (i<=20)
-            System.out.print(i+ " ");
+        while (i<=20) {
+            System.out.println(i+" ");
+            i++;
+        }
+
     }
 }
--- src/homework/day2/cycletask/PrintOddNumbers.java
+++ src/homework/day2/cycletask/PrintOddNumbers.java
@@ -3,6 +3,11 @@ package homework.day2.cycletask;
 public class PrintOddNumbers {
     public void printOddNum (){
 
+        for (int num=3; num<=19; num++){
+            if (num%2!=0) {
+                System.out.println(num);
+            }
+        }
     }
 
 }
