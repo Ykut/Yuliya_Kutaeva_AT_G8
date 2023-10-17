74c43f2c0983313eb9ce701896a417a2791c6268
Ykut <51128539+Ykut@users.noreply.github.com>
2023-10-17 11:44:51 +0300

    Update Transportable.java

--- src/homework/day4/playground/essence/craft/Transportable.java
+++ src/homework/day4/playground/essence/craft/Transportable.java
@@ -1,5 +1,7 @@
 package homework.day4.playground.essence.craft;
 
 public interface Transportable {
-    int move(int pointA, int pointB);
+    default int move(int pointA, int pointB){
+        System.out. println("I am "+this.getClass()+", my name is "+this.getName()+" and I am moving from point "+pointA+" to point "+pointB);
+        return pointB - pointA;
 }
