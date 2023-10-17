dd6fedbdb770d7682aec44fa81797ea379deba76
Ykut <51128539+Ykut@users.noreply.github.com>
2023-10-17 12:28:26 +0300

    Update Task4

--- src/homework/day4/playground/essence/creatures/Fly.java
+++ src/homework/day4/playground/essence/creatures/Fly.java
@@ -7,10 +7,4 @@ public class Fly extends Insect implements Flyable {
     public Fly(int mass, String name) {
         super(mass, name);
     }
-
-    @Override
-    public void fly(String direction) {
-        System.out.println("I am " + super.getClass() + ", my name is " + super.getClass().getName() + " and I amd flying to " + direction);
-
-    }
 }
