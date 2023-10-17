c45a9a014d1358f054e2446fdd22cf90d209320f
Ykut <51128539+Ykut@users.noreply.github.com>
2023-10-17 11:47:14 +0300

    Update Aircraft.java

--- src/homework/day4/playground/essence/craft/air/Aircraft.java
+++ src/homework/day4/playground/essence/craft/air/Aircraft.java
@@ -19,15 +19,4 @@ public abstract class Aircraft extends Matter implements Flyable, Transportable 
     public void setName(String name) {
         this.name = name;
     }
-
-    @Override
-    public void fly(String direction) {
-            System.out.println("I am " + this.getClass() + ", my name is " + this.getName() + " and I amd flying to " + direction);
-    }
-
-    @Override
-    public int move(int pointA, int pointB) {
-        System.out.println("I am "+this.getClass()+", my name is "+this.getName()+" and I am moving from point "+pointA+" to point "+pointB);;
-        return pointB - pointA;
-    }
 }
