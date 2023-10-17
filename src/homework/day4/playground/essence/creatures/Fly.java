f6fe7c7d21a0b83d4dee1f198014c14f3a79d1c2
Ykut <51128539+Ykut@users.noreply.github.com>
2023-10-17 12:29:01 +0300

    Update Task4

--- src/homework/day4/playground/essence/creatures/Fly.java
+++ src/homework/day4/playground/essence/creatures/Fly.java
@@ -4,7 +4,4 @@ import homework.day4.playground.essence.Flyable;
 
 public class Fly extends Insect implements Flyable {
 
-    public Fly(int mass, String name) {
-        super(mass, name);
-    }
 }
