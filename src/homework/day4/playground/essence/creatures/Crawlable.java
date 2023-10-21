7fc135c5ea5b034a0f658bf4e23145ccc3ca438b
Kutaeva_Yuliya <kutaeva.yuliya@gmail.com>
2023-10-21 11:47:19 +0300

    task 6 (Crocodile not "wr-wr-wrr-r..", and Beetle not "vz-vz-vzz-zz..")

--- src/homework/day4/playground/essence/creatures/Crawlable.java
+++ src/homework/day4/playground/essence/creatures/Crawlable.java
@@ -1,4 +1,14 @@
 package homework.day4.playground.essence.creatures;
 
 public interface Crawlable {
+    default void crawl(String direction, int distance){
+        System.out.printf("I am %s, my name is %s and I am crawling to %s for %d units%n", getClass().getSimpleName(), this.getClass(), direction, distance);
+
+        //if ()
+
+    }
+
+
+
+
 }
