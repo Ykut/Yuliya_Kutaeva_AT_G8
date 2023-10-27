fa098f92c2d94f3e5386fd945da44ff921d31299
Kutaeva Yuliya <kutaeva.yuliya@gmail.com>
2023-10-27 16:02:11 +0300

    homework.day8.collectiontask.2

--- src/homework/day8/collectionTask/Figures.java
+++ src/homework/day8/collectionTask/Figures.java
@@ -1,15 +1,21 @@
 package homework.day8.collectionTask;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 public class Figures {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         List<String> figures = Arrays.asList("Овал", "Прямоугольник", "Круг", "Квадрат", "Эллипс");
 
+        BufferedWriter out = new BufferedWriter(new FileWriter("figures.txt"));
         for (String str: figures) {
-            System.out.println(str);//вывести в файл
+            out.write("-"+str);//вывести в файл
         }
+        out.close();
 
         int i=0;
         for(String str2:figures){
@@ -19,16 +25,12 @@ public class Figures {
 
         System.out.println(i);
 
+        
+
         for (String figure : figures) {
             System.out.print(figure + " ");
         }
         System.out.print("\n");
-         try {
-                figures.add(3,"Треугольник");
-         }
-        catch (UnsupportedOperationException e){
-            System.out.println(e.getClass() + " В список figures нельзя добавить элемент");
-        }
 
         for (String str3: figures) {
             System.out.print(str3+" ");
