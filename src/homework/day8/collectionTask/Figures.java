d7394e396048d06b3e6c2eb0e7739f865dfad282
Kutaeva Yuliya <kutaeva.yuliya@gmail.com>
2023-10-27 14:07:51 +0300

    homework.day8.collectiontask.2

---
+++ src/homework/day8/collectionTask/Figures.java
@@ -0,0 +1,39 @@
+package homework.day8.collectionTask;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Figures {
+    public static void main(String[] args) {
+        List<String> figures = Arrays.asList("Овал", "Прямоугольник", "Круг", "Квадрат", "Эллипс");
+
+        for (String str: figures) {
+            System.out.println(str);//вывести в файл
+        }
+
+        int i=0;
+        for(String str2:figures){
+            if(!str2.contains("и"))
+                i++;
+        }
+
+        System.out.println(i);
+
+        for (String figure : figures) {
+            System.out.print(figure + " ");
+        }
+        System.out.print("\n");
+         try {
+                figures.add(3,"Треугольник");
+         }
+        catch (UnsupportedOperationException e){
+            System.out.println(e.getClass() + " В список figures нельзя добавить элемент");
+        }
+
+
+
+        for (String str3: figures) {
+            System.out.print(str3+" ");
+        }
+    }
+}
