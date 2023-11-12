39fababe9fe48c5fff19031b4557798632088b64
Kutaeva Yuliya <kutaeva.yuliya@gmail.com>
2023-11-13 01:25:40 +0300

    homework.day10.Stream

--- src/homework/day10/WaterRunner.java
+++ src/homework/day10/WaterRunner.java
@@ -13,24 +13,25 @@ public class WaterRunner {
         //из 4 объектов воды с цветом (Прозрачная, Прозрачная, Мутная, Синяя)
         // и запахом (Нет, Нет, Аммиачный, Мятный) соответственно
 
-        Stream<Water> water = Stream.of(
+        List<Water> water = Stream.of(
                 new Water("Прозрачная", "Нет"),
                 new Water("Прозрачная", "Нет"),
                 new Water("Мутная", "Аммиачный"),
                 new Water("Синяя", "Мятный")
-        );
+        ).toList();
+
 //Отфильтровать воду, цвет которой не "Прозрачная"
 
-        //water.filter(water1 -> !water1.getColor().equals("Прозрачная")).peek(System.out::println).toList();
+        water.stream().filter(water1 -> !water1.getColor().equals("Прозрачная")).peek(System.out::println).toList();
 
 //Отсортировать в нисходящем порядке по запаху
 
-        //water.sorted((w1,w2)->-w1.getSmell().compareTo(w2.getSmell())).peek(System.out::println).toList();
+        water.stream().sorted((w1, w2) -> -w1.getSmell().compareTo(w2.getSmell())).peek(System.out::println).toList();
 
 //На основании существующего потока данных создать новый, в котором каждая новая Water
 // имеет запах с удвоенной буквой ы, если такая встречается
 
-        List<Water> newWater = water.map(water1 -> {
+        List<Water> newWater = water.stream().map(water1 -> {
             if (water1.getSmell().contains("ы"))
                 water1.setSmell(water1.getSmell().replace("ы", "ыы"));
             return water1;
@@ -39,6 +40,7 @@ public class WaterRunner {
 //Обьединить запах всех обьектов в одну строку
 
         String smellString = newWater.stream().map(Water::getSmell).collect(Collectors.joining());
+        System.out.println(smellString);
 
 //Найти количество буков в ней и отпечатать в консоль
 
